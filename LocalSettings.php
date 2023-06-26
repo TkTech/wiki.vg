4b16c3c8c8a32f4f6ead638f219bcc48a8ede23a
Tyler Kennedy <tk@tkte.ch>
2023-06-25 20:15:32 -0400

    Use the temporary domain name for testing.

--- LocalSettings.php
+++ LocalSettings.php
@@ -29,7 +29,7 @@ $wgArticlePath = "/$1";
 $wgUsePathInfo = true;
 
 # The protocol and server name to use in fully-qualified URLs
-$wgServer = "https://wiki.vg";
+$wgServer = "https://wiki.montreal.tkte.ch";
 
 # The relative URL path to the skins directory
 $wgStylePath = "$wgScriptPath/skins";
